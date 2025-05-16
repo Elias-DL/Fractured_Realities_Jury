6033ac1d4bee5c2c95a74ab76a80ae116ad631dc
Elias-DL <eliaslenchoj.deloore@leerling.campushast.be>
2025-05-16 20:32:30 +0200

    assets : assetimported(3) folder

---
+++ Fractured_Realities_Jury_Unity/Assets/AssetImported/Rusty Flashlight/Scripts/FlashlightToggle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    public GameObject licht;
+    public bool isOn = true;
+
+    void Start()
+    {
+        //start met licht uit
+        licht.SetActive(isOn);
+        licht.GetComponent<Light>().enabled = true;
+
+    }
+
+    void Update()
+    {
+        if (EquippedItemManager.Instance.EquippedItemName == "Flashlight")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                //toggle light
+                isOn = !isOn;
+                //turn light on
+                if (isOn)
+                {
+                    licht.SetActive(true);
+                    licht.GetComponent<Light>().enabled = true;
+                }
+                //turn light off
+                else
+                {
+                    licht.GetComponent<Light>().enabled = false;
+
+                    licht.SetActive(false);
+
+                }
+            }
+
+        }
+
+
+        //Debug.Log(EquippedItemManager.Instance.EquippedItemName);
+    }
+}
